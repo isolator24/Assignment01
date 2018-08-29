a5c1d096c523c7306e58571e9d566fa3c68965aa
Anthony Wong <AnthonyW@magenic.com>
2018-08-29 11:19:57 +0800

    Test

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
-import React from 'react';
-import { Button, View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation'; // Version can be specified in package.json
-import Post from './components/Post.js';
 import HomeScreen from './components/HomeScreen.js';
 import DetailsScreen from './components/DetailsScreen.js';
 import MyCustomScreen from './components/MyCustomScreen.js';
-
-
 
 DetailsScreen
 
